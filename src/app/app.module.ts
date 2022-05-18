83887f852fa83a36aa38a4c2b91db69bcd8fd334
surya-6666 <surya.guthula6666@gmail.com>
2022-05-18 08:31:10 +0530

    My First Commt in BradTraversy Template

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    UserComponent,
+    UsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
